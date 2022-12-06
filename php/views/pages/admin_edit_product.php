4944f8857a237722975ca977f0a521b4b1811f22
nom1 <nom1@email.com>
2022-12-06 16:44:26 +0100

    Edit page done

--- php/views/pages/admin_edit_product.php
+++ php/views/pages/admin_edit_product.php
@@ -1,10 +1,8 @@
 <?php
 $pageTitle = "Admin edit product";
-
 if (isset($_GET['id'])){
     $productId = $_GET['id'];
 }
-
 $product = $ProductManager->get_all_from_table("WHERE id = " . $productId);
 $category = $CategoryManager->get_all_from_table();
 ob_start();
@@ -13,29 +11,29 @@ ob_start();
 <!-- If Admin -->
 <form action="actions/edit.php" method ="POST">
     <label for="name">Name</label>
-    <input type="text" name="name" id="name" value="" required>
+    <input type="text" name="name" id="name" value="<?= $product[0]->name ?>" required>
 
     <label for="description">Description</label>
-    <input type="text" name="description" id="description" value="" required>
+    <input type="text" name="description" id="description" value="<?= $product[0]->description ?>" required>
 
     <label for="price">Price</label>
-    <input type="number" step="0.01" min="0" name="price" id="price" value="" required>
+    <input type="number" step="0.01" min="0" name="price" id="price" value="<?= $product[0]->price ?>" required>
 
     <label for="stock">Stock</label>
-    <input type="number" min="0" name="stock" id="stock" value="" required>
+    <input type="number" min="0" name="stock" id="stock" value="<?= $product[0]->stock ?>" required>
 
     <label for="slug">Slug</label>
-    <input type="text" name="slug" id="slug" value="" required>
+    <input type="text" name="slug" id="slug" value="<?= $product[0]->slug ?>" required>
 
     <label for="category">Catégorie :</label>
     <select name="category" id="category" required>
     <?php foreach ($category as $cat){?>
-        <option value="<?=$cat->id?>" <?php if(false){ echo "selected"; } ?>><?=$cat->name?></option>
+        <option value="<?=$cat->id?>" <?php if($product[0]->category_id == $cat->id){ echo "selected"; } ?>><?=$cat->name?></option>
     <?php }
     ?>
     </select>
-    <input type="hidden" name="product_id" value="">
-    <input type="submit" value ="Modifier" name="edit"  class="submitButton">
+    <input type="hidden" name="product_id" value="<?= $product[0]->id ?>">
+    <input type="submit" value ="Modifier" name="edit" class="submitButton">
 </form>
 <?php
 $pageContent = ob_get_clean();
