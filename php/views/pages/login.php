2eaf4973ea54789bb233f78b9f937d14c02d58d5
nom1 <nom1@email.com>
2022-12-07 11:06:54 +0100

    Setup login/register

--- php/views/pages/login.php
+++ php/views/pages/login.php
@@ -3,6 +3,15 @@ $pageTitle = "Connexion";
 ob_start();
 ?>
 <h1>Connexion</h1>
+<form action = "actions/login.php" method="POST">
+<label for="name">Name:</label><br>
+  <input type="text" id="name" name="name"><br>
+
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password"><br>
+
+  <input type="submit" value="connexion">
+</form>
 <?php
 $pageContent = ob_get_clean();
 ?>
