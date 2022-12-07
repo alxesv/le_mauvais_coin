5ac977f275245170ed4f726b19c148a821d1e02f
nom1 <nom1@email.com>
2022-12-07 09:20:59 +0100

    Updated list to use classes

--- php/views/pages/list.php
+++ php/views/pages/list.php
@@ -1,13 +1,13 @@
 <?php
 $pageTitle = "Liste";
-$sql = "SELECT product.name as 'Name', product.description as 'Description', product.price as 'Price', category.name as 'Category', product.id as 'id', product.slug as 'slug' FROM product
-INNER JOIN category ON product.category_id = category.id";
+$sql = null;
 if (isset($_GET['search'])) {
     $searchContent = $_GET['search'];
-    $research = " WHERE product.name LIKE '%$searchContent%'";
-    $sql .= $research;
+    $research = " WHERE name LIKE '%$searchContent%'";
+    $sql = $research;
 }
-$products = $ProductManager->custom_select($sql);
+$product = $ProductManager->get_all_from_table($sql);
+$category = $CategoryManager->get_all_from_table();
 ob_start();
 ?>
 <h1>Liste</h1>
@@ -19,15 +19,15 @@ ob_start();
         </thead>
         <tbody>
         <?php
-            foreach ($products as $pkey => $pvalue){ ?>
+            foreach ($product as $p){ ?>
                 <tr>
-                    <td><a href="?p=product&slug=<?= $pvalue['slug'] ?>"><?=$pvalue['Name']?></a></td>
-                    <td><?=$pvalue['Description']?></td>
-                    <td><?=$pvalue['Category']?></td>
-                    <td><?=$pvalue['Price']?></td>
+                    <td><a href="?p=product&slug=<?= $p->slug ?>"><?=$p->name?></a></td>
+                    <td><?=$p->description?></td>
+                    <td><?php foreach($category as $c){if($c->id == $p->category_id){ echo $c->name;}} ?></td>
+                    <td><?=$p->price?></td>
                     <!-- If admin -->
-                    <td><a href="?p=admin_edit_product&id=<?=$pvalue['id']?>">Modifier</a></td>
-                    <td><form method="POST" action ="actions/delete.php"><input type="hidden" name="product_id" value="<?=$pvalue['id']?>"><input type="submit" name="delete" value="Supprimer" class="deleteButton"></form></td>
+                    <td><a href="?p=admin_edit_product&id=<?=$p->id?>">Modifier</a></td>
+                    <td><form method="POST" action ="actions/delete.php"><input type="hidden" name="product_id" value="<?=$p->id?>"><input type="submit" name="delete" value="Supprimer" class="deleteButton"></form></td>
                 <tr>
                 <?php
                     }
