5ccd33250c0abb68c0fd12995837decbffbceb73
nom1 <nom1@email.com>
2022-12-08 15:15:55 +0100

    js et css bootstrap

--- php/views/partials/footer.php
+++ php/views/partials/footer.php
@@ -2,6 +2,6 @@
 <p>Besoin d'aide ?</p>
 <p>Contactez nous au 06.26.12.06.66 😁</p>
 </footer>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
