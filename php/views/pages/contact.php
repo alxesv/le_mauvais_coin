780aa8d014191f4c225793547b84938413ba9f6f
nom2 <nom2@email.com>
2022-12-09 10:42:04 +0100

    css

--- php/views/pages/contact.php
+++ php/views/pages/contact.php
@@ -4,7 +4,7 @@ $pageTitle = "Contact - LMC";
 $error_message = get_error();
 ob_start();
 ?>
-<h1>Contact page</h1>
+<h1 class="text-center main-title">Contact page</h1>
 
 <?php 
 if ($error_message){ ?>
@@ -14,7 +14,8 @@ if ($error_message){ ?>
 <?php if($is_admin) {?>
   <a href="?p=admin_contact">Admin contact page</a>
 <?php } ?>
-<form action="actions/send_contact.php" method="POST">
+<section class="form-inscription">
+<form class="form-note" action="actions/send_contact.php" method="POST">
   <label for="name">Votre nom :</label><br>
   <input type="text" id="name" name="name"><br>
   
@@ -22,10 +23,11 @@ if ($error_message){ ?>
   <input type="email" id="email" name="email"><br>
   
   <label for="message">Votre message :</label><br>
-  <textarea id="message" name="message"></textarea><br>
+  <textarea class= "message-contact" id="message" name="message"></textarea><br>
   
-  <button type="submit">Envoyer</button>
+  <button class = "button-inscription btn btn-primary" type="submit" value="connexion">Envoyer</button>
 </form>
+</section>
 <?php
 $pageContent = ob_get_clean();
 ?>
