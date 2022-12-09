6590c09230537ea0bba6c5a1c457862593c776e5
nom1 <nom1@email.com>
2022-12-09 13:41:22 +0100

    total prix commande, mes commandes

--- php/views/pages/mes_commandes.php
+++ php/views/pages/mes_commandes.php
@@ -10,10 +10,11 @@ ob_start();
 ?>
 <h1>Mes commandes</h1>
 <?php foreach($commandes as $commande){ 
+    $prixTotal = 0;
     $productCommande = $ProductCommandeManager->get_all_from_table("WHERE commande_id =" . $commande->id); 
     ?>
     <div class="p-3 mb-2 bg-light text-dark shadow-sm">
-    <div>Numéro de commande <?= $commande->id ?></div>
+    <div class="text-center">Numéro de commande <?= $commande->id ?></div>
     <div>Statut : <?= $commande->status ?></div>
     <div>Commandé le : <?= $commande->dateCommande ?></div>
     <div>Contenu : 
@@ -21,6 +22,7 @@ ob_start();
             <?php foreach($productCommande as $pc){
                 $products = $ProductManager->get_all_from_table("WHERE id =" . $pc->product_id);
                 foreach($products as $product){
+                    $prixTotal += $pc->quantity*$product->price;
                 ?>
                 <li class="list-group-item d-flex justify-content-between align-items-center">Produit : <?= $product->name?></li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">Description : <?= $product->description ?></li>
@@ -30,6 +32,7 @@ ob_start();
             <?php } }?>
         </ul>
     </div>
+    <div class="text-center">Prix total : <?= $prixTotal ?> $</div>
     </div>
 <?php } ?>
     
