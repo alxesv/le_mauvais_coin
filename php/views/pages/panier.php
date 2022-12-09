523253921011180e2591eecd6e0f24a716d2a5f3
nom1 <nom1@email.com>
2022-12-09 13:43:29 +0100

    total prix commande, panier

--- php/views/pages/panier.php
+++ php/views/pages/panier.php
@@ -6,11 +6,13 @@ if(isset($_SESSION['user_id'])){
 $query= "SELECT product.id, panier.quantity, product.name, product.price, product.slug FROM panier INNER JOIN product ON panier.product_id = product.id WHERE panier.user_id =" .$user_id;
 $panier = $PanierManager->custom_select($query);
 $items= $PanierManager->get_all_from_table("WHERE user_id =" . $user_id); 
-
+$totalPanier = 0;
 ob_start();
 ?>
 <h1>Mon panier</h1>
-<?php foreach($panier as $productPanier){ ?>
+<?php foreach($panier as $productPanier){
+        $totalPanier+= $productPanier['price'] * $productPanier['quantity'];
+        ?>
     <div class="p-3 mb-2 bg-light text-dark shadow-sm">
             <li><a href = "?p=product&slug=<?=$productPanier["slug"] ?>" ><?= $productPanier["name"] ?></a> : <?= $productPanier["price"] ?> $</li>
         <div class="btn-group btn-panier">
@@ -22,6 +24,7 @@ ob_start();
 <?php }  if(empty($items)){?>
 <p>Le panier est vide.</p>
 <?php } else { ?>
+    <div class="text-center">Prix total : <?= $totalPanier ?> $</div>
     <a href="?p=commande">
     <input type="submit" name="commande" value="Commander" class="commandeButton btn btn-primary"></a>
 <?php
