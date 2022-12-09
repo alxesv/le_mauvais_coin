66646f7bb73156684a40a1b2dff35cc76b8b9021
nom1 <nom1@email.com>
2022-12-09 13:35:39 +0100

    total prix commande

--- php/views/pages/commande.php
+++ php/views/pages/commande.php
@@ -6,6 +6,7 @@ if(isset($_SESSION['user_id'])){
 $error_message = get_error();
 $query= "SELECT product.id, product.name, product.price , product.description, panier.quantity FROM panier INNER JOIN product ON panier.product_id = product.id WHERE panier.user_id =". $user_id;
 $panier = $PanierManager->custom_select($query);
+$totalCommande = 0;
 ob_start();
 ?>
 <h1>Ma commande</h1>
@@ -13,13 +14,16 @@ ob_start();
 <h2>Récapitulatif</h2>
 <?php 
 foreach($panier as $productPanier){ ?>
-
+    <?php $totalCommande += $productPanier['quantity'] * $productPanier['price'] ?>
     <div class="list-group">
-    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $productPanier["name"] ?> : <?= $productPanier["price"] ?> $ </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $productPanier["description"] ?> </li></div></br>
+    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $productPanier["name"] ?> : <?= $productPanier["price"] ?> $</li>        
+    <li class="list-group-item d-flex justify-content-between align-items-center">Quantité : <?= $productPanier['quantity']?></li>
+    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $productPanier["description"] ?> </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">Total : <?= $productPanier['quantity'] * $productPanier['price']?> $</li>
+</div></br>
       
 <?php } ?>
-
+<div class="text-center">Total de la commande : <?= $totalCommande?> $</div>
 <form action= "actions/addCommande.php" method="POST" >
 <?php if($error_message){ ?>    
     <p class= "error"><?= $error_message?></p>
